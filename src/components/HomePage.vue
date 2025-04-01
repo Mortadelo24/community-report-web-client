c6e8246a19093893307d68831a35d4f450d02b4d
Mortadelo24 <josedanielmirandaperez410@gmail.com>
2025-04-01 15:47:37 -0600

    crud

---
+++ src/components/HomePage.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+  import {ref} from 'vue';
+  const count = ref(0);
+  
+</script>
+
+<template>
+  <div>
+   Hello home page 
+   <p>Counter: {{count}}</p>
+  </div>
+</template>
