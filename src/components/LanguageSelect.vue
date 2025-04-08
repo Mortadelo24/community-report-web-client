ee921159b7a2807bdd3a3d4e36e1f2f4e5b63ac2
Mortadelo24 <josedanielmirandaperez410@gmail.com>
2025-04-07 22:02:59 -0600

    add selectLanguage

--- src/components/LanguageSelect.vue
+++ src/components/LanguageSelect.vue
@@ -1,16 +1,26 @@
 <script setup lang="ts">
 import {useLanguageStore} from '../stores/language.ts'
 import {storeToRefs} from 'pinia'
+import {supportedLanguages} from '../languages/index.ts'
+import type {LanguageCode} from '../languages/index.ts'
+import {ref, watch} from 'vue'
 
 
 const languageStore = useLanguageStore();
 
+const {loadLanguagePack} = languageStore;
 const {preference} = storeToRefs(languageStore);
- 
+const selectedLanguage = ref<keyof LanguageCode>(preference.value);
+
+watch(selectedLanguage, ()=> {
+  loadLanguagePack(selectedLanguage.value); 
+})
 </script>
 
 <template>
   <div>
-  <p>Selected language: {{preference}}</p>  
+    <select v-model="selectedLanguage" >
+      <option v-for="language in supportedLanguages" :value="language">{{language}}</option>
+    </select> 
   </div>
 </template>
--- src/languages/index.ts
+++ src/languages/index.ts
@@ -31,6 +31,7 @@ export type {
 export {
   getLanguagePack,
   getLanguagePreference,
+  supportedLanguages
   
   
 }
