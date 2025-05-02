6774067d911e2dda1ff158a89dca672fcc04dc06
Mortadelo24 <josedanielmirandaperez410@gmail.com>
2025-05-02 10:19:07 -0600

    fix the fix

--- src/modules/user/services/firebase.ts
+++ src/modules/user/services/firebase.ts
@@ -1,8 +1,15 @@
 import {initializeApp} from 'firebase/app'
 import { getAuth, GoogleAuthProvider, signInWithPopup} from "firebase/auth";
-import firebaseConfig from "@/../firebaseConfig.json"
 
-const app = initializeApp(firebaseConfig);
+
+const app = initializeApp({
+  "apiKey": "AIzaSyB3PA7IzJDkaNcEfo3Fnu27fr-P10r1AT8",
+  "authDomain": "integrador-communities.firebaseapp.com",
+  "projectId": "integrador-communities",
+  "storageBucket": "integrador-communities.firebasestorage.app",
+  "messagingSenderId": "382672568496",
+  "appId": "1:382672568496:web:62a91735c6c0d01f063b75"
+});
 const auth = getAuth(app);
 
 const login = async() => { 
